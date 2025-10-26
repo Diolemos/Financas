c6bd5430040a0bf82c2467a08a7727e0316f5d50
Dio Lemos <67412764+Diolemos@users.noreply.github.com>
2025-10-26 20:33:32 -0300

    update models

--- app/src/main/java/org/pedro/model/Wallet.java
+++ app/src/main/java/org/pedro/model/Wallet.java
@@ -1,13 +1,33 @@
 package org.pedro.model;
 
 
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
+
 import lombok.Getter;
 import lombok.ToString;
 
 @ToString
 public abstract  class Wallet {
     @Getter
-    private final BankService BankServiceType;
+    private final BankService service;
 
     protected final List<Money> money;
+
+    public Wallet(final BankService serviceType){
+        this.service = serviceType;
+        this.money = new ArrayList<>();
+    }
+
+    protected  List<Money> generateMoney(final long amount, final String description){
+            var history = new MoneyAudit(UUID.randomUUID(), service, description, OffsetDateTime.now());
+            return Stream.generate(()->new Money(history))
+            .limit(amount).toList();
+    }
+    public long getFunds(){
+        return money.size();
+    }
 }
